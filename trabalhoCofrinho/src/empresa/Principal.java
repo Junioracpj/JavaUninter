d285a474f1b3fd9eef58e6369285fd5b91090a5e
Antonio Carlos Pereira Junior <100253996+Junioracpj@users.noreply.github.com>
2022-10-28 20:08:11 -0300

    Trocando IF por Switch Case
    Resolvi realizar a troca de condicionante afim de deixar o código mais limpo. Também comecei a implementar comentários dentro das classes.

--- trabalhoCofrinho/src/empresa/Principal.java
+++ trabalhoCofrinho/src/empresa/Principal.java
@@ -7,17 +7,17 @@ public class Principal {
 	
 	public static void MenuPrincipal(){
 		System.out.println("COFRINHO");
-		System.out.println("1 - Adcionar Moeda /n"
-				+ "2 - Remover Moeda /n"
-				+ "3 -Listar Moeda /n"
+		System.out.println("1 - Adcionar Moeda \n"
+				+ "2 - Remover Moeda \n"
+				+ "3 - Listar Moeda \n"
 				+ "4 - Calcular total convertido para Real"
-				+ "0 -Encerrar");
+				+ "0 - Encerrar");
 	}
 	public static void MenuMoedas(){
 	System.out.println("Escolha a moeda: ");
-	System.out.println("1 - Real /n"
-			+ "2 - Dolar /n"
-			+ "3 - Euro /n");
+	System.out.println("1 - Real \n"
+			+ "2 - Dolar \n"
+			+ "3 - Euro \n");
 	}
 	
 	public static void main(String[] args) {
@@ -25,33 +25,47 @@ public class Principal {
 		ArrayList<Moeda> listaMoedas = new ArrayList<Moeda>();
 		
 		Scanner teclado = new Scanner(System.in);
-		boolean fim = false;
-		int resposta;
-		
+		int opcao;
+		MenuPrincipal();
+		opcao = teclado.nextInt();
 
-		while (!fim) {
-			MenuPrincipal();
-			resposta = teclado.nextInt();
-			if(resposta == 1) {
+		while (opcao!=0) {
+			switch(opcao) {
+			case 1: // Adcionar Moedas
+				int opcao2;
 				MenuMoedas();
-				resposta = teclado.nextInt();
-				if(resposta == 01) {
-					listaMoedas.add(new Euro(teclado.nextDouble()));
-				}else if(resposta == 02) {
+				opcao2 = teclado.nextInt();
+				switch(opcao2) {
+				case 1: // Adcionar Euro
+					listaMoedas.add(new Euro(teclado.nextDouble()));	
+					break;
+				case 2:	// Adcionar Dolar
 					listaMoedas.add(new Dolar(teclado.nextDouble()));
-				}else {
+					break;
+				case 3: // Adcionar Real
 					listaMoedas.add(new Real(teclado.nextDouble()));
+					break;
+				default:
+					System.out.println("Opção invalida,tente novamente");
+					MenuMoedas();
+					opcao = teclado.nextInt();
 				}
-			}else if(resposta == 2) {
+			case 2: // Remover Moedas
 				listaMoedas.remove(teclado.nextInt());
-			}else if(resposta == 3) {
-					listaMoedas.toString();
-			}else if(resposta == 4) {
-//				enconstrar solucao para o calculo	
-			}else {
-				System.out.println("Encerrando");
-				fim = true;
+				break;
+			case 3: // Listar Moedas
+				listaMoedas.toString();
+				break;
+			case 4: // Calcular valor total em reais
+				double total = 0;
+				for(Moeda c : listaMoedas) {
+					total += c.calculaValor();
+					System.out.println("Valor total das moedas "
+							+ "convertidas em real :" + total);}
+				break;
+			default:
+				System.out.println("Opção invalida,tente novamente");
 			}
-	}
+		}
 }
 }
